c1700c4c5a4dad0379796ac2f3ba53c95f5b86a9
Evgeniy Dobrygin <sarecud@gmail.com>
2025-08-31 13:28:01 +0700

    Fix: rename class game to Game to match file name

--- game.cs
+++ game.cs
@@ -1,104 +1,104 @@
 using Godot;
 using System;
 
-public partial class game : Node2D
+public partial class Game : Node2D
 {
-	Vector2 screenSize;
-	Vector2 padSize;
-	Vector2 direction = new Vector2(1, 1);
+   Vector2 screenSize;
+   Vector2 padSize;
+   Vector2 direction = new Vector2(1, 1);
 
-	const int InitialBallSpeed = 80;
-	double ballSpeed = InitialBallSpeed;
-	int padSpeed = 150;
+   const int InitialBallSpeed = 80;
+   double ballSpeed = InitialBallSpeed;
+   int padSpeed = 150;
    int leftScore = 0;
    int rightScore = 0;
-	public override void _Ready()
-	{
-		screenSize = GetViewportRect().Size;
-		padSize = GetNode<Sprite2D>("left").Texture.GetSize();
+   public override void _Ready()
+   {
+      screenSize = GetViewportRect().Size;
+      padSize = GetNode<Sprite2D>("left").Texture.GetSize();
 
-		SetProcess(true);
-	}
+      SetProcess(true);
+   }
 
-	public override void _Process(double delta)
+   public override void _Process(double delta)
 
-	{
+   {
       var hud = GetNode<hud>("HUD");
-      
-		var time = 0.0;
-		time += delta;
 
-		var ballPos = GetNode<Sprite2D>("ball").Position;
-		var leftRect = new Rect2(GetNode<Sprite2D>("left").Position - padSize / 2, padSize);
-		var rightRect = new Rect2(GetNode<Sprite2D>("right").Position - padSize / 2, padSize);
+      var time = 0.0;
+      time += delta;
 
-		// Integrate new ball position
-		ballPos += direction * (float)ballSpeed * (float)delta;
+      var ballPos = GetNode<Sprite2D>("ball").Position;
+      var leftRect = new Rect2(GetNode<Sprite2D>("left").Position - padSize / 2, padSize);
+      var rightRect = new Rect2(GetNode<Sprite2D>("right").Position - padSize / 2, padSize);
 
-		// Flip when touching roof	or floor
-		if ((ballPos.Y < 0 && direction.Y < 0) || (ballPos.Y > screenSize.Y && direction.Y > 0))
-		{
-			direction.Y = -direction.Y;
-		}
+      // Integrate new ball position
+      ballPos += direction * (float)ballSpeed * (float)delta;
 
-		// Flip, change direction and increace speed when touching pads
-		if ((leftRect.HasPoint(ballPos) && direction.X < 0) || (rightRect.HasPoint(ballPos) && direction.X > 0))
-		{
-			direction.X = -direction.X;
-			direction.Y = GD.Randf() * 2 - 1;
-			direction = direction.Normalized();
-			ballSpeed *= 1.1;
-		}
+      // Flip when touching roof	or floor
+      if ((ballPos.Y < 0 && direction.Y < 0) || (ballPos.Y > screenSize.Y && direction.Y > 0))
+      {
+         direction.Y = -direction.Y;
+      }
 
-      if(ballPos.X > screenSize.X)
+      // Flip, change direction and increace speed when touching pads
+      if ((leftRect.HasPoint(ballPos) && direction.X < 0) || (rightRect.HasPoint(ballPos) && direction.X > 0))
+      {
+         direction.X = -direction.X;
+         direction.Y = GD.Randf() * 2 - 1;
+         direction = direction.Normalized();
+         ballSpeed *= 1.1;
+      }
+
+      if (ballPos.X > screenSize.X)
       {
          leftScore++;
          hud.UpdateScoreLeft(leftScore);
       }
 
-      if(ballPos.X < 0)
+      if (ballPos.X < 0)
       {
          rightScore++;
          hud.UpdateScoreRight(rightScore);
       }
 
-		// Check gameover
-		if (ballPos.X < 0 || ballPos.X > screenSize.X)
-		{
-			ballPos = screenSize / 2;
-			ballSpeed = InitialBallSpeed;
-			direction = new Vector2(-1, -1);
-		}
-      
+      // Check gameover
+      if (ballPos.X < 0 || ballPos.X > screenSize.X)
+      {
+         ballPos = screenSize / 2;
+         ballSpeed = InitialBallSpeed;
+         direction = new Vector2(-1, -1);
+      }
 
-		GetNode<Sprite2D>("ball").Position = ballPos;
 
-		// Move left pad
-		var leftPos = GetNode<Sprite2D>("left").Position;
+      GetNode<Sprite2D>("ball").Position = ballPos;
 
-		if (leftPos.Y > 0 && Input.IsActionPressed("leftMoveUp"))
-		{
-			leftPos.Y += -padSpeed * (float)delta;
-		}
-		if (leftPos.Y < screenSize.Y && Input.IsActionPressed("leftMoveDown"))
-		{
-			leftPos.Y += padSpeed * (float)delta;
-		}
+      // Move left pad
+      var leftPos = GetNode<Sprite2D>("left").Position;
 
-		GetNode<Sprite2D>("left").Position = leftPos;
+      if (leftPos.Y > 0 && Input.IsActionPressed("leftMoveUp"))
+      {
+         leftPos.Y += -padSpeed * (float)delta;
+      }
+      if (leftPos.Y < screenSize.Y && Input.IsActionPressed("leftMoveDown"))
+      {
+         leftPos.Y += padSpeed * (float)delta;
+      }
 
-		// Move right pad
-		var rightPos = GetNode<Sprite2D>("right").Position;
+      GetNode<Sprite2D>("left").Position = leftPos;
 
-		if (rightPos.Y > 0 && Input.IsActionPressed("rightMoveUp"))
-		{
-			rightPos.Y += -padSpeed * (float)delta;
-		}
-		if (rightPos.Y < screenSize.Y && Input.IsActionPressed("rightMoveDown"))
-		{
-			rightPos.Y += padSpeed * (float)delta;
-		}
+      // Move right pad
+      var rightPos = GetNode<Sprite2D>("right").Position;
 
-		GetNode<Sprite2D>("right").Position = rightPos;
-	}
+      if (rightPos.Y > 0 && Input.IsActionPressed("rightMoveUp"))
+      {
+         rightPos.Y += -padSpeed * (float)delta;
+      }
+      if (rightPos.Y < screenSize.Y && Input.IsActionPressed("rightMoveDown"))
+      {
+         rightPos.Y += padSpeed * (float)delta;
+      }
+
+      GetNode<Sprite2D>("right").Position = rightPos;
+   }
 }
